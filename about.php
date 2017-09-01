a75f83149bc233cace4be55078aba94229e98b61
sumiprajapati <sumiprajapati26@gmail.com>
2017-09-01 18:54:11 +0545

    added all folder once

---
+++ about.php
@@ -0,0 +1,2 @@
+<h2>About Us</h2>
+<h3>This online notice board is a place where students get notified about different activities of college.Different notice related to exam routines,dates and result publication dates are notified in their respective account as well as students are notified about their holidays through this notice board.</h3>
